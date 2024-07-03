4c3bcfcf2e2e39561adcd182360f5950c46b42d2
unknown <onureredo@yahoo.com>
2024-07-03 14:38:40 +0200

    Ä

--- client/src/components/SearchForm/SearchForm.jsx
+++ client/src/components/SearchForm/SearchForm.jsx
@@ -3,9 +3,8 @@ import { FaSearch } from 'react-icons/fa';
 import { useNavigate } from 'react-router-dom';
 import { useGlobalContext } from '../../context';
 
-
 const SearchForm = () => {
-  // const { setSearchTerm, setResultTitle } = useGlobalContext();
+  const { setSearchTerm, setResultTitle } = useGlobalContext();
   const searchText = useRef('');
   const navigate = useNavigate();
 
@@ -14,13 +13,13 @@ const SearchForm = () => {
   const handleSubmit = (e) => {
     e.preventDefault();
     let tempSearchTerm = searchText.current.value.trim();
-    if ((tempSearchTerm.replace(/[^\w\s]/gi, "")).length === 0) {
-      setSearchTerm("Harry potter");
-      setResultTitle("Please Enter Something ...");
+    if (tempSearchTerm.replace(/[^\w\s]/gi, '').length === 0) {
+      setSearchTerm('');
+      setResultTitle('Please Enter Something ...');
     } else {
       setSearchTerm(searchText.current.value);
     }
-    navigate("/book");
+    navigate('/book');
   };
 
   return (
@@ -30,12 +29,15 @@ const SearchForm = () => {
           <form className='search-form' onSubmit={handleSubmit}>
             <div className='search-form-elem flex items-center bg-white rounded-full px-7 py-4'>
               <input
-                type="text"
+                type='text'
                 className='form-control flex-grow text-xl px-4 py-2 border-2 border-gray-300 rounded-full'
-                placeholder='Harry potter .....'
+                placeholder='Search...'
                 ref={searchText}
               />
-              <button type="submit" className='flex items-center justify-center ml-4'>
+              <button
+                type='submit'
+                className='flex items-center justify-center ml-4'
+              >
                 <FaSearch className='text-purple text-2xl' />
               </button>
             </div>
@@ -44,8 +46,6 @@ const SearchForm = () => {
       </div>
     </div>
   );
-}
+};
 
 export default SearchForm;
-
-
