2a238fcd565a9a9b78f5a5cf191ffc09c921522a
Hajar Alipourchoopany <162975571+Hajar-Alipourchoopany@users.noreply.github.com>
2024-07-05 15:06:17 +0200

    Update routes.js

--- Backend/route/routes.js
+++ Backend/route/routes.js
@@ -12,10 +12,10 @@ router.get('/books/:isbn', getBookByISBN);
 // UC02: Neues Buch erfassen
 router.post('/books', upload.single('book_image'), addNewBook);
 
-// UC03: Rezension zu einem Buch hinzufügen
+// UC03: review zu einem Buch hinzufügen
 router.post('/reviews', addReview);
 
-// UC07: Rezension löschen
+// UC07: review löschen
 router.delete('/reviews/:reviewId', deleteReview);
 
 // UC04: Benutzer registrieren
@@ -27,7 +27,11 @@ router.post('/login', loginUser);
 // UC05: Benutzer ausloggen
 router.post('/logout', logoutUser);
 
-// UC06: Rezensionen eines Benutzers anzeigen
+// UC06: review eines Benutzers anzeigen
 router.get('/users/:userId/reviews', getUserReviews);
+
+router.get('/topreviewer', getTopReviewer);
+
+router.post('/:userId/favorites', addReviewToFavorites);
 
 export default router;
